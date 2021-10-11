9c2b5af53cc9e10cce4ab0bcd6fd2e81a85228f9
Chetan Temkar <58204548+ctemkar@users.noreply.github.com>
2021-10-11 01:47:12 -0400

    fixed problems

--- index.js
+++ index.js
@@ -12,12 +12,4 @@ express()
 
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-showTimes = () => {
-  let result = '';
-  const times = process.env.TIMES || 5;
-  for (i = 0; i < times; i++) {
-    result += i + ' ';
-  }
-  return result;
-};
 
